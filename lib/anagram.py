e8cf644a9c2013bf97f650ce31564f5599dbd5f7
Jory Roberts <robertsjory13@gmail.com>
2023-07-17 13:09:11 -0500

    completes lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,24 @@
 # your code goes here!
+class Anagram:
+  def __init__(self, word):
+    self.word = word
+
+  def match(self, list):
+    wordlist = []
+
+    first_word = [letter for letter in self.word]
+    first_word.sort()
+
+    for word in list:
+      second_word = [letter for letter in word]
+      second_word.sort()
+
+      if first_word == second_word:
+        wordlist.append(word)
+
+    self.list = wordlist
+    return wordlist
+
+
+
+
